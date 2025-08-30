3acef1721c83ca56d8a8bdf2c323932afaebae29
Shivraj Soni <shivrajsoni09022005@gmail.com>
2025-08-30 23:20:34 +0530

    added response and request implementation

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    io::{BufRead, BufReader, Read, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -11,6 +11,61 @@ pub struct Request {
     pub headers: HashMap<String, String>,
     pub query: HashMap<String, String>,
     pub content: String,
+}
+
+#[derive(Debug)]
+pub struct Response {
+    status_text: String,
+    headers: Vec<(String, String)>,
+    body: String,
+}
+
+impl Response {
+    pub fn json(status: u16, body: &str, headers: Option<Vec<(String, String)>>) -> Self {
+        // conversion to response code
+        let content_len = body.len();
+        let predetermined_headers = vec![
+            ("Content-Type".to_string(), "application/json".to_string()),
+            ("Content-Length".to_string(), content_len.to_string()),
+        ];
+
+        let headers = headers.unwrap_or_else(|| vec![]);
+
+        let status_text = match status {
+            200 => "200 OK".to_string(),
+            400 => "400 Bad Request".to_string(),
+            500 => "500 Internal Server Error".to_string(),
+            _ => format!("{} Unknown ", status),
+        };
+
+        Self {
+            status_text,
+            headers: [predetermined_headers, headers].concat(),
+            body: body.to_string(),
+        }
+    }
+
+    pub fn resolve(response: &Response) -> String {
+        // response to string code
+        let mut response_str = format!(
+            "HTTP/1.1 {}
+",
+            response.status_text
+        );
+
+        for (key, value) in &response.headers {
+            response_str.push_str(&format!(
+                "{}: {}
+",
+                key, value
+            ));
+        }
+
+        response_str.push_str("\r\n");
+        response_str.push_str(&response.body);
+
+        response_str
+    }
 }
 
 const MESSAGE_SIZE: usize = 1024;
@@ -32,59 +87,68 @@ impl Request {
                 }
                 Err(err) => {
                     println!("Error : {:#?}", err);
-                }
-            }
-
-            let request_text = String::from_utf8(recieved).unwrap();
-
-            let mut request_lines: Vec<&str> = request_text.split_inclusive('\n').collect();
-
-            let mut header_map: HashMap<String, String> = HashMap::new();
-            let mut query_params: HashMap<String, String> = HashMap::new();
-
-            // GET /path?x=1 HTTP/1.1
-
-            let request_line = request_lines[0];
-            let mut parts = request_line.split_ascii_whitespace();
-
-            let http_method = parts.next().unwrap();
-            let full_path = parts.next().unwrap();
-
-            // seperate path and query string
-            let path_and_query: Vec<&str> = full_path.split('?').collect();
-
-            let path = path_and_query[0];
-
-            if path_and_query.len() > 1 {
-                let query_string = path_and_query[1..].join("");
-                let query_pairs: Vec<&str> = query_string.split("&").collect();
-                for pairs in query_pairs {
-                    if let Some((key, value)) = pairs.split_once("=") {
-                        query_params.insert(key.to_string(), value.to_string());
-                    }
-                }
-            }
-
-            // we have manipulated the first line of request lines so we can remove it
-            request_lines.remove(0);
-
-            let blank_line_index = request_lines
-                .iter()
-                .position(|&line| line == "\r\n")
-                .unwrap();
-            let body_lines = &mut request_lines.split_off(blank_line_index);
-            body_lines.remove(0); // remove the blank line itself  
-            let body_content = body_lines.join("");
-
-            for header_line in &request_lines {
-                if header_line.trim().is_empty() {
-                    continue;
-                }
-                if let Some((key, value)) = header_line.split_once(": ") {
-                    header_map.insert(key.to_string(), value.trim().to_string());
+                    return Err(err.to_string());
                 }
             }
         }
+
+        let request_text = String::from_utf8(recieved).unwrap();
+
+        let mut request_lines: Vec<&str> = request_text.split_inclusive('\n').collect();
+
+        let mut header_map: HashMap<String, String> = HashMap::new();
+        let mut query_params: HashMap<String, String> = HashMap::new();
+
+        // GET /path?x=1 HTTP/1.1
+
+        let request_line = request_lines[0];
+        let mut parts = request_line.split_ascii_whitespace();
+
+        let http_method = parts.next().unwrap().to_string();
+        let full_path = parts.next().unwrap();
+
+        // seperate path and query string
+        let path_and_query: Vec<&str> = full_path.split('?').collect();
+
+        let path = path_and_query[0].to_string();
+
+        if path_and_query.len() > 1 {
+            let query_string = path_and_query[1..].join("");
+            let query_pairs: Vec<&str> = query_string.split("&").collect();
+            for pairs in query_pairs {
+                if let Some((key, value)) = pairs.split_once("=") {
+                    query_params.insert(key.to_string(), value.to_string());
+                }
+            }
+        }
+
+        // we have manipulated the first line of request lines so we can remove it
+        request_lines.remove(0);
+
+        let blank_line_index = request_lines
+            .iter()
+            .position(|&line| line == "\r\n")
+            .unwrap();
+        let body_lines = &mut request_lines.split_off(blank_line_index);
+        body_lines.remove(0); // remove the blank line itself
+        let body_content = body_lines.join("");
+
+        for header_line in &request_lines {
+            if header_line.trim().is_empty() {
+                continue;
+            }
+            if let Some((key, value)) = header_line.split_once(": ") {
+                header_map.insert(key.to_string(), value.trim().to_string());
+            }
+        }
+
+        Ok(Self {
+            method: http_method,
+            path,
+            headers: header_map,
+            query: query_params,
+            content: body_content,
+        })
     }
 }
 
@@ -114,26 +178,31 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-
-    // Read the request line
-    let request_line = buf_reader.lines().next();
-
-    // Ensure request_line is not None and unwrap it
-    if let Some(Ok(request)) = request_line {
-        println!("Request: {}", request);
-
-        // A simple response
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = "<h1>Hello, from your Rust server!</h1>";
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
-        println!("Response sent.");
-    } else {
-        eprintln!("Failed to read request line.");
+    let req = Request::new(&stream);
+    let res = match req {
+        Ok(req) => match req.path.as_str() {
+            "/hello" => {
+                let def_name = String::from("Shivraj");
+                let name: &String = req.query.get("name").unwrap_or_else(|| &def_name);
+                let payload = format!("{{\"message\": \"Hello, {}!\"}}", name);
+                Response::json(200, &payload, None)
+            }
+            _ => {
+                let payload = "{{\"message\": \"Invalid Path\"}}";
+                Response::json(400, &payload, None)
+            }
+        },
+        Err(e) => {
+            let payload = format!("{{\"message\": \"Error: {}}}", e);
+            Response::json(500, &payload, None)
+        }
+    };
+    let response_str = Response::resolve(&res);
+    match stream.write(response_str.as_bytes()) {
+        Ok(_) => {}
+        Err(_) => {
+            println!("FAILED DISPATCHED RESPONSE");
+        }
     }
 }
 
